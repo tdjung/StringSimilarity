b50b4fdad78d95ff1c05ca6d0d84902ff8d2e03d
tdjung <td.jung@samsung.com>
2025-07-02 16:54:34 +0900

    [refactoring] first alpha refactoring update

--- string_similarity_test.cpp
+++ string_similarity_test.cpp
@@ -8,50 +8,42 @@ public:
 	void lengthCheck(double expectPoint, const string& firstString, const string& secondString) {
 		EXPECT_DOUBLE_EQ(expectPoint, sc.lengthPoint(firstString, secondString));
 	}
+
+	void alphaCheck(double expectPoint, const string& firstString, const string& secondString) {
+		EXPECT_DOUBLE_EQ(expectPoint, sc.alphaPoint(firstString, secondString));
+	}
 };
 
-TEST_F(StringCheckerFixture, string1) {
+TEST_F(StringCheckerFixture, lenghCheckString1) {
 	lengthCheck(60,"ASD", "DSA");
 }
 
-TEST_F(StringCheckerFixture, string2) {
+TEST_F(StringCheckerFixture, lenghCheckString2) {
 	lengthCheck(0 ,"A", "BB");
 }
 
-TEST_F(StringCheckerFixture, string3) {
+TEST_F(StringCheckerFixture, lenghCheckString3) {
 	lengthCheck(20, "AAABB", "BAA");;
 }
 
-TEST_F(StringCheckerFixture, string4) {
+TEST_F(StringCheckerFixture, lenghCheckString4) {
 	lengthCheck(30, "AA", "AAE");
 }
 
-TEST(StringCheckAlpha, tc1) {
-	StringChecker sc;
-
-	double alpha_point = sc.alphaPoint("ASD", "DSA");
-	EXPECT_DOUBLE_EQ(40, alpha_point);
+TEST_F(StringCheckerFixture, alphaCheckString1) {
+	alphaCheck(40, "ASD", "DSA");
 }
 
-TEST(StringCheckAlpha, tc2) {
-	StringChecker sc;
-
-	double alpha_point = sc.alphaPoint("A", "BB");
-	EXPECT_DOUBLE_EQ(0, alpha_point);
+TEST_F(StringCheckerFixture, alphaCheckString2) {
+	alphaCheck(0 , "A", "BB");
 }
 
-TEST(StringCheckAlpha, tc3) {
-	StringChecker sc;
-
-	double alpha_point = sc.alphaPoint("AAABB", "BA");
-	EXPECT_DOUBLE_EQ(40, alpha_point);
+TEST_F(StringCheckerFixture, alphaCheckString3) {
+	alphaCheck(40, "AAABB", "BA");
 }
 
-TEST(StringCheckAlpha, tc4) {
-	StringChecker sc;
-
-	double alpha_point = sc.alphaPoint("AA", "AAE");
-	EXPECT_DOUBLE_EQ(20, alpha_point);
+TEST_F(StringCheckerFixture, alphaCheckString4) {
+	alphaCheck(20, "AA", "AAE");
 }
 
 int main() {
